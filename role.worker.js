6a843c68bcec495e6d2a79f6872a1e829bdd7848
Darkosto <darkosto@darkosto.tv>
2021-01-20 13:48:33 -0800

    Fixed workers not identifying controller

--- role.worker.js
+++ role.worker.js
@@ -2,29 +2,27 @@ var role_worker = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        
-            console.log(creep.store[RESOURCE_ENERGY] , creep.store.getCapacity());
-            if(creep.store[RESOURCE_ENERGY] < creep.store.getCapacity()) {
+
+        console.log(creep.store[RESOURCE_ENERGY], creep.store.getCapacity());
+        if (creep.store[RESOURCE_ENERGY] < creep.store.getCapacity()) {
             var sources = creep.room.find(FIND_SOURCES_ACTIVE);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
 
             }
-        }
-       
-        else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_CONTROLLER) && structure.energy < structure.energyCapacityAvailable;
-                                //structure.structureType == STRUCTURE_EXTENSION
-                                //structure.structureType == STRUCTURE_CONTAINER ||
-                                //structure.structureType == STRUCTURE_SPAWN ||
-                                //structure.structureType == STRUCTURE_TOWER
-                    }
+        } else {
+            var targets = creep.room.find(FIND_MY_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_CONTROLLER) && structure.progress  < structure.progressTotal;
+                    //structure.structureType == STRUCTURE_EXTENSION
+                    //structure.structureType == STRUCTURE_CONTAINER ||
+                    //structure.structureType == STRUCTURE_SPAWN ||
+                    //structure.structureType == STRUCTURE_TOWER
+                }
             });
             console.log(targets)
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            if (targets.length > 0) {
+                if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
                 }
             }
