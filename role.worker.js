78d31f025635412f5f534e62d5c03c5d1f49e146
Darkosto <darkosto@darkosto.tv>
2021-01-20 14:47:24 -0800

    Fixed worker gathering

--- role.worker.js
+++ role.worker.js
@@ -12,24 +12,18 @@ var role_worker = {
             }
         } 
         */
+        console.log(creep.store[RESOURCE_ENERGY], creep.store.getCapacity());
         if (creep.memory.working && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.working = false;
             creep.say('collecting!');
         }
-        if (!creep.memory.working && creep.store[RESOURCE_ENERGY], creep.store.getCapacity()) {
+
+        if (!creep.memory.working && creep.store[RESOURCE_ENERGY] == 50) {
             creep.memory.working = true;
-            creep.say('energizing!');
+            creep.say('test!');
         }
 
 
-
-        if (creep.memory.working) {
-            var sources = creep.room.find(FIND_SOURCES_ACTIVE);
-            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-
-            }
-        }
 
         if (creep.memory.working) {
             var targets = creep.room.find(FIND_MY_STRUCTURES, {
@@ -40,13 +34,18 @@ var role_worker = {
                     //structure.structureType == STRUCTURE_SPAWN ||
                     //structure.structureType == STRUCTURE_TOWER
                 }
-
             });
             console.log(targets)
+
             if (targets.length > 0) {
                 if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
                 }
+            }
+        } else {
+            var sources = creep.room.find(FIND_SOURCES_ACTIVE);
+            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
             }
         }
     }
