32f8dd86190666d8d8fb004bb4ba6a4f18663d55
Darkosto <darkosto@darkosto.tv>
2021-01-21 20:53:45 -0800

    Fix constructor not moving out of working role when construction exists

--- role.constructor.js
+++ role.constructor.js
@@ -1,42 +1,41 @@
 var role_constructor = {
-
-    /** @param {Creep} creep **/
-    run: function(creep) {
-
-        if(creep.memory.building && creep.carry.energy == 0) {
-            creep.memory.building = false;
-            creep.say('harvesting');
-        }
-        if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-            creep.memory.building = true;
-            creep.say('building');
-        }
-
-        if(creep.memory.building) {
-            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
-                targets.sort(function (a, b) {
-                    return (a.progress/a.progressTotal - b.progress/b.progressTotal);
-                });
-                if(creep.build(targets[targets.length-1]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[targets.length-1]);
-                }
-            }
-        }
-        
-        if(creep.room.find(FIND_MY_CONSTRUCTION_SITES) == 0) {
-           creep.memory.role = 'worker';
-           creep.say('Switching to Working!');
-        }
-
-        
-        else {
-            var sources = creep.room.find(FIND_SOURCES_ACTIVE);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
-        }
+  /** @param {Creep} creep **/
+  run: function (creep) {
+    if (creep.memory.building && creep.carry.energy == 0) {
+      creep.memory.building = false;
+      creep.say("harvesting");
     }
+    if (!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
+      creep.memory.building = true;
+      creep.say("building");
+      creep.say("Test!");
+    }
+
+    var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+    if (targets.length == 0) {
+      creep.say("BROKE");
+      creep.memory.role = "worker";
+      creep.say("Switching to Working!");
+    }
+
+    if (creep.memory.building) {
+      var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+      if (targets.length) {
+        creep.memory.working = false;
+        targets.sort(function (a, b) {
+          return a.progress / a.progressTotal - b.progress / b.progressTotal;
+        });
+        if (creep.build(targets[targets.length - 1]) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(targets[targets.length - 1]);
+        }
+      }
+    } else {
+      var sources = creep.room.find(FIND_SOURCES_ACTIVE);
+      if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0]);
+      }
+    }
+  },
 };
 
 module.exports = role_constructor;
