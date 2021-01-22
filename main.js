e04a59005ed5d39c6f469c1644143d208bea0773
Darkosto <darkosto@darkosto.tv>
2021-01-21 20:53:26 -0800

    Rename roles

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ var role_worker = require('role.worker');
 var role_worker_midpoint = require('role.worker2');
 var role_constructor = require('role.constructor');
 var role_repairer = require('role.repairer');
+var role_scout = require('role.scout');
 
 module.exports.loop = function() {
-
 
     // Spawn New Worker
     var workers = _.filter(Game.creeps, (creep) => creep.memory.role == 'worker');
 
-    if (workers.length < 4) {
-        var newName = Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE, MOVE], 'Worker' + (Math.floor(Math.random() * 65534) + 1), {
+    if (workers.length < 6) {
+        var newName = Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'worker' + (Math.floor(Math.random() * 65534) + 1), {
             memory: {
                 role: 'worker'
             }
@@ -19,19 +19,16 @@ module.exports.loop = function() {
     }
 
     // Spawn New Midpoint Worker
+    var midworkers = _.filter(Game.creeps, (creep) => creep.memory.role == 'midworker');
 
-    var mpworkers = _.filter(Game.creeps, (creep) => creep.memory.role == 'mpworker');
-
-    if (mpworkers.length < 1) {
+    if (midworkers.length < 1) {
         var newName = Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE, MOVE], 'MidPointWorker' + (Math.floor(Math.random() * 65534) + 1), {
             memory: {
-                role: 'mpworker'
+                role: 'midworker'
             }
         });
-        console.log('Spawning new mpworker: ' + newName);
+        console.log('Spawning new midworker: ' + newName);
     }
-
-
     // Construction
     var constructors = _.filter(Game.creeps, (creep) => creep.memory.role == 'constructor');
     //var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
@@ -47,26 +44,38 @@ module.exports.loop = function() {
     }
 
     // Repairer
-    var repairer = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
+    var repairs = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
     //var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
 
     //if(repairer.length < 3 && harvesters.length > 2 && ConstructionSite.length > 0) {
-    if (repairer.length < 1) {
-        var newName = Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE, MOVE], 'repairer' + (Math.floor(Math.random() * 65534) + 1), {
+    if (repairs.length < 2) {
+        var newName = Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'repairer' + (Math.floor(Math.random() * 65534) + 1), {
             memory: {
                 role: 'repairer'
             }
         });
         console.log('Spawning new repairer: ' + newName);
     }
-
-
+    ////
+    // Scout
+    ////
+    var scouts = _.filter(Game.creeps, (creep) => creep.memory.role == 'scout');
+    //if(capturer.length < 3 && harvesters.length > 2 && ConstructionSite.length > 0) {
+    if (scouts.length < 1) {
+        var newName = Game.spawns['Spawn1'].spawnCreep([MOVE, CLAIM, MOVE], 'scout' + (Math.floor(Math.random() * 65534) + 1), {
+            memory: {
+                role: 'scout'
+            }
+        });
+        console.log('Spawning new scout: ' + newName);
+    }
+    
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
         if (creep.memory.role == 'worker') {
             role_worker.run(creep);
         }
-        if (creep.memory.role == 'mpworker') {
+        if (creep.memory.role == 'midworker') {
             role_worker_midpoint.run(creep);
         }
         //if(creep.memory.role == 'upgrader') {
@@ -77,6 +86,9 @@ module.exports.loop = function() {
         }
         if (creep.memory.role == 'repairer') {
             role_repairer.run(creep);
+        }
+        if (creep.memory.role == 'scout') {
+            role_scout.run(creep);
         }
 
     }
